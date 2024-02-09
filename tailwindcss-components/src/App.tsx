e35392742a95f27cac63b2a015fb57278863a6f8
Ignacio Diaz <informatica.diaz@gmail.com>
2024-02-09 11:19:28 -0300

    add component

--- tailwindcss-components/src/App.tsx
+++ tailwindcss-components/src/App.tsx
@@ -4,6 +4,12 @@ import './App.css'
 
 import SlideOver from './components/Ecommerce/ShoppingCarts/SlideOver'
 import StoreNavigation from './components/Ecommerce/StoreNavigation'
+import Footer from './components/Footer/Footer'
+import ProductList from './components/Ecommerce/ProductList'
+import ProducOverviews from './components/Ecommerce/ProductOverviews'
+import PromoSection from './components/Ecommerce/PromoSection'
+
+
 
 
 
@@ -21,6 +27,11 @@ function App() {
     <>
       <StoreNavigation {...{ cartOpen, setCartOpen }} />
       <SlideOver  {...{ cartOpen, setCartOpen }} />
+      <PromoSection />
+      <ProductList />
+      <ProducOverviews />
+
+      <Footer />
     </>
   )
 }
