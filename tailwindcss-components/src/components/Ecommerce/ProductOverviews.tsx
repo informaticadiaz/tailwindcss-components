092736a5a9e9ce0e1a1042bf45d97c84b267fc81
Ignacio Diaz <informatica.diaz@gmail.com>
2024-03-03 21:50:44 -0300

    type: string

--- tailwindcss-components/src/components/Ecommerce/ProductOverviews.tsx
+++ tailwindcss-components/src/components/Ecommerce/ProductOverviews.tsx
@@ -77,7 +77,7 @@ const product = {
 }
 const reviews = { href: '#', average: 4, totalCount: 117 }
 
-function classNames(...classes) {
+function classNames(...classes: string[]) {
   return classes.filter(Boolean).join(' ')
 }
 
