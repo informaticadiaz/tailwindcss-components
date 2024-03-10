52ccdca483830d22019435044e07fbbe13db7b71
Ignacio Diaz <informatica.diaz@gmail.com>
2024-03-10 19:26:01 -0300

    configure prettier para que ordene las clases de tailwindcss

--- tailwindcss-components/src/App.tsx
+++ tailwindcss-components/src/App.tsx
@@ -1,21 +1,17 @@
-import './App.css'
+import "./App.css";
 
-import SlideOver from './components/Ecommerce/ShoppingCarts/SlideOver'
-import StoreNavigation from './components/Ecommerce/StoreNavigation'
-import Footer from './components/Footer/Footer'
-import ProductList from './components/Ecommerce/ProductList'
-import ProducOverviews from './components/Ecommerce/ProductOverviews'
-import PromoSection from './components/Ecommerce/PromoSection'
-import Search from './components/Utils/Search/Search'
-import Blog from './components/Blog/Blog'
-import ThreeCoumn from './components/Blog/ThreeColumn'
-import ThreecolumnmBGImg from './components/Blog/ThreecolumnmBGImg'
-import SingleColmn from './components/Blog/SingleColmn'
-
-
-
-
-
+import SlideOver from "./components/Ecommerce/ShoppingCarts/SlideOver";
+import StoreNavigation from "./components/Ecommerce/StoreNavigation";
+import Footer from "./components/Footer/Footer";
+import ProductList from "./components/Ecommerce/ProductList";
+import ProducOverviews from "./components/Ecommerce/ProductOverviews";
+import PromoSection from "./components/Ecommerce/PromoSection";
+import Search from "./components/Utils/Search/Search";
+import Blog from "./components/Blog/Blog";
+import ThreeCoumn from "./components/Blog/ThreeColumn";
+import ThreecolumnmBGImg from "./components/Blog/ThreecolumnmBGImg";
+import SingleColmn from "./components/Blog/SingleColmn";
+import SingleColumnImg from "./components/Blog/SingleColumnImg";
 
 function App() {
   // return (
@@ -34,12 +30,13 @@ function App() {
         throw new Error('Function not implemented.')
       } } />
     <Footer />*/}
-    {/* <Blog /> */}
+      {/* <Blog /> */}
       {/* <ThreeCoumn /> */}
       {/* <ThreecolumnmBGImg /> */}
-      <SingleColmn />
+      {/* <SingleColmn /> */}
+      <SingleColumnImg />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
---
+++ tailwindcss-components/src/components/Blog/SingleColumnImg.tsx
@@ -0,0 +1,185 @@
+// https://tailwindui.com/components/marketing/sections/blog-sections#component-69c177c9d4921026ce3ddd0c3f6846b0
+
+export default function SingleColumnImg() {
+  return (
+    <div className="bg-white py-24 sm:py-32">
+      <div className="mx-auto max-w-7xl px-6 lg:px-8">
+        <div className="mx-auto max-w-2xl lg:max-w-4xl">
+          <h2 className="text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">
+            From the blog
+          </h2>
+          <p className="mt-2 text-lg leading-8 text-gray-600">
+            Learn how to grow your business with our expert advice.
+          </p>
+          <div className="mt-16 lg:mt-20">
+            <article className="relative isolate my-24 flex flex-col gap-8 space-y-20 lg:flex-row">
+              <div className="relative aspect-[16/9] shrink-0 sm:aspect-[2/1] lg:aspect-[1/1] lg:w-64 ">
+                <img
+                  src="./work.webp"
+                  alt=""
+                  className="absolute inset-0 h-full w-full rounded-2xl bg-gray-50 object-cover"
+                />
+                <div className="absolute inset-0 rounded-2xl ring-1 ring-inset ring-gray-900/10"></div>
+              </div>
+              <div>
+                <div className="flex items-center gap-x-4 text-xs">
+                  <time dateTime="2020-03-16" className="text-gray-500">
+                    Mar 16, 2020
+                  </time>
+                  <a
+                    href="#"
+                    className="relative z-10 rounded-full bg-gray-50 px-3 py-1.5 font-medium text-gray-600 hover:bg-gray-100"
+                  >
+                    Marketing
+                  </a>
+                </div>
+                <div className="relative max-w-xl">
+                  <h3 className="mt-3 text-lg font-semibold leading-6 text-gray-900">
+                    <a href="#">
+                      <span className="absolute inset-0" />
+                      Boost your conversion rate
+                    </a>
+                  </h3>
+                  <p className="mt-5 text-sm leading-6 text-gray-600">
+                    Illo sint voluptas. Error voluptates culpa eligendi. Hic vel
+                    totam vitae illo. Non aliquid explicabo necessitatibus unde.
+                    Sed exercitationem placeat consectetur nulla deserunt vel
+                    iusto corrupti dicta laboris incididunt.
+                  </p>
+                </div>
+                <div className="mt-6 flex border-t border-gray-900/5 pt-6">
+                  <div className="relative flex items-center gap-x-4">
+                    <img
+                      src="./work.webp"
+                      alt=""
+                      className="h-10 w-10 rounded-full bg-gray-50"
+                    />
+                    <div className="text-sm leading-6">
+                      <p className="font-semibold text-gray-900">
+                        <a href="#">
+                          <span className="absolute inset-0" />
+                          Michael Foster
+                        </a>
+                      </p>
+                      <p className="text-gray-600">Product Designer</p>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </article>
+            <article className="relative isolate my-24 flex flex-col gap-8 lg:flex-row lg:space-y-20">
+              <div className="relative aspect-[16/9] sm:aspect-[2/1] lg:aspect-[1/1] lg:w-64 lg:shrink-0 ">
+                <img
+                  src="./work.webp"
+                  alt=""
+                  className="absolute inset-0 h-full w-full rounded-2xl bg-gray-50 object-cover"
+                />
+                <div className="absolute inset-0 rounded-2xl ring-1 ring-inset ring-gray-900/10"></div>
+              </div>
+              <div>
+                <div className="flex items-center gap-x-4 text-xs">
+                  <time dateTime="2020-03-16" className="text-gray-500">
+                    Mar 16, 2020
+                  </time>
+                  <a
+                    href="#"
+                    className="relative z-10 rounded-full bg-gray-50 px-3 py-1.5 font-medium text-gray-600 hover:bg-gray-100"
+                  >
+                    Marketing
+                  </a>
+                </div>
+                <div className="relative max-w-xl">
+                  <h3 className="mt-3 text-lg font-semibold leading-6 text-gray-900">
+                    <a href="#">
+                      <span className="absolute inset-0" />
+                      Boost your conversion rate
+                    </a>
+                  </h3>
+                  <p className="mt-5 text-sm leading-6 text-gray-600">
+                    Illo sint voluptas. Error voluptates culpa eligendi. Hic vel
+                    totam vitae illo. Non aliquid explicabo necessitatibus unde.
+                    Sed exercitationem placeat consectetur nulla deserunt vel
+                    iusto corrupti dicta laboris incididunt.
+                  </p>
+                </div>
+                <div className="mt-6 flex border-t border-gray-900/5 pt-6">
+                  <div className="relative flex items-center gap-x-4">
+                    <img
+                      src="./work.webp"
+                      alt=""
+                      className="h-10 w-10 rounded-full bg-gray-50"
+                    />
+                    <div className="text-sm leading-6">
+                      <p className="font-semibold text-gray-900">
+                        <a href="#">
+                          <span className="absolute inset-0" />
+                          Michael Foster
+                        </a>
+                      </p>
+                      <p className="text-gray-600">Product Designer</p>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </article>
+            <article className="relative isolate my-24 flex flex-col gap-8 lg:flex-row lg:space-y-20">
+              <div className="relative aspect-[16/9] sm:aspect-[2/1] lg:aspect-[1/1] lg:w-64 lg:shrink-0 ">
+                <img
+                  src="./work.webp"
+                  alt=""
+                  className="absolute inset-0 h-full w-full rounded-2xl bg-gray-50 object-cover"
+                />
+                <div className="absolute inset-0 rounded-2xl ring-1 ring-inset ring-gray-900/10"></div>
+              </div>
+              <div>
+                <div className="flex items-center gap-x-4 text-xs">
+                  <time dateTime="2020-03-16" className="text-gray-500">
+                    Mar 16, 2020
+                  </time>
+                  <a
+                    href="#"
+                    className="relative z-10 rounded-full bg-gray-50 px-3 py-1.5 font-medium text-gray-600 hover:bg-gray-100"
+                  >
+                    Marketing
+                  </a>
+                </div>
+                <div className="relative max-w-xl">
+                  <h3 className="mt-3 text-lg font-semibold leading-6 text-gray-900">
+                    <a href="#">
+                      <span className="absolute inset-0" />
+                      Boost your conversion rate
+                    </a>
+                  </h3>
+                  <p className="mt-5 text-sm leading-6 text-gray-600">
+                    Illo sint voluptas. Error voluptates culpa eligendi. Hic vel
+                    totam vitae illo. Non aliquid explicabo necessitatibus unde.
+                    Sed exercitationem placeat consectetur nulla deserunt vel
+                    iusto corrupti dicta laboris incididunt.
+                  </p>
+                </div>
+                <div className="mt-6 flex border-t border-gray-900/5 pt-6">
+                  <div className="relative flex items-center gap-x-4">
+                    <img
+                      src="./work.webp"
+                      alt=""
+                      className="h-10 w-10 rounded-full bg-gray-50"
+                    />
+                    <div className="text-sm leading-6">
+                      <p className="font-semibold text-gray-900">
+                        <a href="#">
+                          <span className="absolute inset-0" />
+                          Michael Foster
+                        </a>
+                      </p>
+                      <p className="text-gray-600">Product Designer</p>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </article>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
